9572010c39096a8abdbdedfa2ba65f71f84dc804
amirg <a.m.gholizad@gmail.com>
2026-09-11 09:44:23 -0230

    Add Amir Gholizad's resume in LaTeX format

--- geo.js
+++ geo.js
@@ -24,8 +24,7 @@
   const NL_PHONE_TEXT = "+1 709 691 2883";
   const NL_PHONE_HREF = "tel:+17096912883";
   const NL_LOCATION = "St. John's, NL, Canada";
-  const STJOHNS_RESUME =
-    "https://docs.google.com/document/d/1jTm8_Ek8gDyGtRG_h53pabuT_HQoU5hIE4WmUrD4H9w/edit?tab=t.0";
+  const STJOHNS_RESUME = "assets/files/Amir_Gholizad_Resume_St_Johns.pdf";
 
   function applyNewfoundland() {
     if (phoneEl) {
